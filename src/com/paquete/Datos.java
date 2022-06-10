3ac64cb6e5ca296807b58443b87fdcf291b86477
Diego C <delcaos12@gmail.com>
2022-06-09 23:18:18 -0500

    Primer commit del proyecto

---
+++ src/com/paquete/Datos.java
@@ -0,0 +1,19 @@
+package com.paquete;
+
+public class Datos {
+
+    public static void main(String[] args) {
+        int entero = 40;
+        long largo = 1220;
+        double doble = 199.988;
+        boolean booleano = true;
+        String cadena = "Esto es una cadena de texto";
+
+        System.out.println(entero);
+        System.out.println(largo);
+        System.out.println(doble);
+        System.out.println(booleano);
+        System.out.println(cadena);
+    }
+
+}
